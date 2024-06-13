e302d2c023a427e49c3fac14dc4d3d43604c764e
64160077 <64160077@go.buu.ac.th>
2024-06-13 10:21:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import Page1 from './Page1';
 import Page2 from './Page2';
+import './App.css'
 
 const App = () => {
   // return (
@@ -17,15 +18,29 @@ const App = () => {
   //     </Routes>
   //   </BrowserRouter>
   // );
+  const [display, setDisplay] = useState(true);
 
   return (
     <BrowserRouter>
-      <nav style={{ fontSize: '30px', backgroundColor: '#2A303C', padding: '20px 10px' }}>
-        <Link to="page1" style={{  marginRight: '20px', textDecoration: 'none', color: 'white'}}>Page 1</Link>
-        <span style={{ marginRight: '20px', color: 'white'}}>|</span>
-        <Link to="page2" style={{  marginRight: '20px', textDecoration: 'none', color: 'white'}}>Page 2</Link>
+      <nav style={{ fontSize: '30px', backgroundColor: '#2A303C', padding: '20px 10px', display: 'flex', justifyContent: 'space-between' }}>
+        <div>
+          <Link to="page1" style={{  marginRight: '20px', textDecoration: 'none', color: 'white'}} onClick={() => setDisplay(false)}>Page 1</Link>
+          <span style={{ marginRight: '20px', color: 'white'}}>|</span>
+          <Link to="page2" style={{  marginRight: '20px', textDecoration: 'none', color: 'white'}} onClick={() => setDisplay(false)}>Page 2</Link>
+        </div>
+        <div style={{  marginRight: '10px'}}>
+          <a href="https://mermaid.js.org/intro/getting-started.html" style={{ textDecoration: 'none', color: 'white' }} target="_blank">Mermaid Doc.</a>
+        </div>
       </nav>
 
+      {
+        display? 
+        <>
+          <div style={{ margin: '20px 0 0 20px' }} className='btn'>
+            <p style={{ fontSize: '40px' }} className='words'> Welcome..! </p>
+          </div>
+        </> : ''
+      }
       <div style={{ margin: '15px 0 0 10px'}}>
         <Routes>
           <Route path='/page1' element={<Page1/>} />
